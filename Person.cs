0c4acfc88312f84ffd3a4542606dbcb93a4e212c
Levente Major <leventemajor36@gmail.com>
2023-10-02 10:04:26 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Person
+    {
+        //A forrás fájlban a mezők jelentése: name, age, city, department, position, gender, marital status, salary (EUR)
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+
+    }
+}
