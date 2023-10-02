03725e4f3eb6b6b048c163a169eed570e6a50fd6
Levente Major <leventemajor36@gmail.com>
2023-10-02 10:24:19 +0200

    File read

--- Person.cs
+++ Person.cs
@@ -14,6 +14,21 @@ namespace Employee
         public string City { get; set; }
         public string Department { get; set; }
         public string Position { get; set; }
+        public bool Gender { get; set; }
+        public bool MaritalStatus { get; set; }
+        public int Salary { get; set; }
 
+        public Person (string ReadLine)
+        {
+            var Pieces = ReadLine.Split(';');
+            this.Name = Pieces[0];
+            this.Age = int.Parse(Pieces[1]);
+            this.City = Pieces[2];
+            this.Department = Pieces[3];
+            this.Position = Pieces[4];
+            this.Gender = Pieces[5] == "Male";
+            this.MaritalStatus = Pieces[6] == "Married";
+            this.Salary = int.Parse(Pieces[7]);
+        }
     }
 }
